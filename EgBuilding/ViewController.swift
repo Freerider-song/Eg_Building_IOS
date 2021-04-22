38b5e43dddb1a94ea27524cee3bb771e370b03dd
Freerider-song <ghthd97@naver.com>
2021-04-22 11:10:59 +0900

    Initial Commit

---
+++ EgBuilding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EgBuilding
+//
+//  Created by (주)에너넷 on 2021/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
